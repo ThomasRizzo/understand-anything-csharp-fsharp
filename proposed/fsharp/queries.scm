112b468d3a0a6550ae7576fe0bacc49039fc0b50
Thomas <tomrizzo@gmail.com>
2026-06-21 13:00:59 -0400

    Improve F# queries.scm with more F#-specific constructs and better comments

--- proposed/fsharp/queries.scm
+++ proposed/fsharp/queries.scm
@@ -1,24 +1,35 @@
 ; proposed/fsharp/queries.scm
-; Tree-sitter queries for F# (community grammar dependent)
+; Tree-sitter queries for F# (based on ionide/tree-sitter-fsharp grammar)
+; Note: F# grammar is still evolving (WIP). These queries target common stable nodes.
+; Focus on practical extractions for knowledge graph: modules, types, functions, bindings.
 
-; Module / Namespace
+; ============================================
+; DEFINITIONS
+; ============================================
+
+; Modules and Namespaces
 (module_declaration
   name: (_) @definition.module)
 
-; Type / Record / Discriminated Union definitions
+; Type definitions (records, discriminated unions, classes, interfaces, structs)
 (type_definition
   name: (identifier) @definition.type)
 
-; Function / Member definitions
-(function_declaration
-  name: (identifier) @definition.function)
+; Function and value definitions
+(function_or_value_defn
+  (identifier) @definition.function)
 
+; Let bindings (very common in F#)
+(let_binding
+  pattern: (identifier) @definition.binding)
+
+; Member definitions (in types)
 (member_declaration
   name: (identifier) @definition.member)
 
-; Let bindings (common in F#)
-(let_binding
-  pattern: (identifier) @definition.variable)
+; ============================================
+; REFERENCES & RELATIONSHIPS
+; ============================================
 
 ; Open / Use directives
 (open_directive
@@ -28,10 +39,34 @@
 (application_expression
   (long_identifier) @reference.call)
 
-; TODO: Add patterns for:
-; - Discriminated union cases
-; - Record fields
-; - Computation expressions (let!, do!)
-; - Attributes
-; - Type providers
-; - Active patterns
+; ============================================
+; F#-SPECIFIC CONSTRUCTS (start simple)
+; ============================================
+
+; Discriminated union cases (very important in F#)
+(union_case
+  name: (identifier) @definition.union_case)
+
+; Record fields
+(record_field
+  name: (identifier) @definition.record_field)
+
+; Computation expressions / workflows (let!, do!, return! etc.)
+; These are powerful in F# - capture the keyword for now
+(computation_expression) @computation.expression
+
+; Attributes (less common than C# but still used)
+(attribute
+  name: (identifier) @reference.attribute)
+
+; ============================================
+; NOTES FOR IMPLEMENTATION
+; ============================================
+; - F# grammar node names can differ slightly between .fs and .fsi files.
+; - Start with basic module + type + function extraction.
+; - The LLM layer will help interpret functional patterns.
+; - Future improvements:
+;     * Better handling of active patterns
+;     * Type provider recognition
+;     * Computation expression workflow extraction
+;     * .fsproj parsing for project references
